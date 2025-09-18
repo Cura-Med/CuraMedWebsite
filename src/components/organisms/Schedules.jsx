06a257c97b5a63e1f31cedd76ee5248fa7f2fa65
margusengso <margusengso@hotmail.com>
2025-09-18 18:37:22 +0300

    doctor Dashboard progress

--- src/components/organisms/Schedules.jsx
+++ src/components/organisms/Schedules.jsx
@@ -6,7 +6,12 @@ import React from "react";
 const Schedules = (schedules) => {
     return (
         <div className='scheduless-wrapper'>
-            <h2>Upcoming Schedules</h2>
+            <div className='schedules-header'>
+                <h2>Upcoming Schedules</h2>
+                <div style={{flex: 1}}/>
+                <div>Add</div>
+            </div>
+
             {!schedules?.[0]?.id &&
                 <p>No upcoming schedules</p>
             }
